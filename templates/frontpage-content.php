6538d9e716f0fca0dc8e8874c3d5203d3bfacd8b
Sicco van Sas <siccovansas@users.noreply.github.com>
2018-03-30 15:46:49 +0200

    Some changes to the frontpage banner and content

--- templates/frontpage-content.php
+++ templates/frontpage-content.php
@@ -1,45 +1,31 @@
 <div>
   <div class="banner2">
     <div class="row">
-      <div class="diggingfordata col-lg-12">
+      <div class="diggingfordata col-md-offset-2 col-md-10">
         <div class="centered">DIGGING FOR DATA</div>
         <div class="centered2">KICK-OFF EVENT: 20 APRIL</div>
       </div>
     </div>
   </div>
-  <hr style="background-color: #cc9800; height:25px;">
+  <br>
+  <br>
+  <hr class="bg-yellow">
   <div class="info-text">
     <div class="row">
-      <div class="col-xs-12 col-md-3">
+      <div class="col-md-offset-2 col-md-6">
+        <h2>Digitale Indiana Jones Gezocht</h2>
+        <div class="frontpage-content-text">
+          De provincie Zuid-Holland is een rijk archeologisch gebied. Archeologie vertelt veel over onze geschiedenis. De provincie Zuid-Holland wil samen met anderen erfgoed helpen ontdekken, beschermen en zorgen dat iedereen er kennis mee kan maken daarom wordt de Digging for Data App Challenge georganiseerd. Ben jij geïnteresseerd in zowel archeologie als data? Heb jij een fantastisch idee voor een archeologie applicatie? Meld je dan aan voor de kick-off meeting.
+        </div>
+        <br>
+        <span>
+          <a href="https://docs.google.com/forms/d/e/1FAIpQLSfbk0gRlvv_ihfB3_aJpoNBAgwJn8cq4PzUuyuN6knmSYlvXA/viewform?usp=sf_link" target="_blank" class="btn btn-registration" role="button">Meld je aan</a>
+        </span>
+        <br>
+        <br>
       </div>
-      <div class="col-xs-12 col-md-6">
-        <p class="frontpage-content-text">
-          <h1>Digitale Indiana Jones gezocht! Doe mee met de app challenge</h1>
-          <h4>De provincie Zuid-Holland is een rijk archeologisch gebied. Archeologie vertelt veel over onze geschiedenis. De provincie Zuid-Holland wil samen met anderen erfgoed helpen ontdekken, beschermen en zorgen dat iedereen er kennis mee kan maken. Archeologie is daarvan een mooi voorbeeld. <br><br>
-
-          De provincie Zuid-Holland stelt haar archeologische objecten, zoals Romeinse munten, 3D schalen en opgravingsdocumentatie, beschikbaar als open data. Hiermee zijn duizenden objecten, afbeeldingen en documenten beschikbaar voor hergebruik. De provincie Zuid-Holland daagt jou daarom uit om deze digitale schatkamer te hergebruiken tijdens de Digging for Data app challenge. Het winnende idee ontvangt van de provincie Zuid-Holland €20.000 ontwikkelbudget om de app daadwerkelijk te kunnen ontwikkelen en bouwen! <br><br>
-
-          Tijdens de Digging for Data app challenge worden ontwikkelaars uitgenodigd om een uitgewerkt app-idee in te sturen. Het idee moet als uitgangspunt hebben om het publieksbereik van archeologische vondsten te vergroten. Archeologie moet fascineren, inspireren en de applicatie moet natuurlijk ook een educatieve meerwaarde hebben door gebruikers van de app kennis te laten maken met de unieke archeologische stukken van de provincie Zuid-Holland. <br><br>
-
-          Heb jij affiniteit met zowel archeologie als datawetenschap en informatica? Heb jij een fantastisch idee voor een archeologie applicatie en kan jij het ook daadwerkelijk uitvoeren? Kortom, ben jij Indiana Jones 2.0 en wil jij met jouw ‘ontdekking’ kans maken op €20.000 ontwikkelbudget? Kom dan 20 april 2018 naar de kick-off meeting bij het COMM Museum in Den Haag en kom alles te weten over de app challenge. </h4>
-        </p>
-        <div class="align-center">
-          <span><a href="https://docs.google.com/forms/d/e/1FAIpQLSfbk0gRlvv_ihfB3_aJpoNBAgwJn8cq4PzUuyuN6knmSYlvXA/viewform?usp=sf_link" class="btn btn-registration" role="button">Meld je aan</a></span> <br><br>
-        </div> 
-
-          Na de kick-off kan iedereen zijn of haar app-idee inzenden tot en met 13 juni. <br><br>
-
-          Details kick-off meeting:<br>
-          Datum: vrijdag 20 april 2018<br>
-          Tijd: 12.30 - 15.30 event<br>
-          15.30 - 17.30 borrel<br>
-          Locatie: COMM Museum <br>
-          Zeestraat 82<br>
-          2518 AD Den Haag<br><br>
-          Tijdens de kick-off meeting krijg je meer informatie over de databronnen en de app challenge. Na de presentaties is er een brainstormsessie en een borrel om de creativiteit met een hapje en een drankje aan te wakkeren. Kijk voor het volledige programma onder het kopje ‘app challenge’. <br><br>
-      </div>
-     <div class="col-xs-12 col-md-3 nieuwsberichtenplaatje"> 
-      <img class="img-responsive block" src="<?= get_template_directory_uri() . '/dist/images/welkomplaatje.jpg' ?>" alt="Gemstone with a carving of two horses by a trough, Roman period. Foto van de Provincie Zuid-Holland">
+     <div class="col-md-3 nieuwsberichtenplaatje">
+      <img class="img-responsive" src="<?= get_template_directory_uri() . '/dist/images/welkomplaatje.jpg' ?>" alt="Gemstone with a carving of two horses by a trough, Roman period. Foto van de Provincie Zuid-Holland">
      </div>
     </div>
   </div>
