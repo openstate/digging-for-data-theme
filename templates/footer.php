f95b24d4b4cb3b2ba8409ea05d4eacee2b219b15
Sicco van Sas <siccovansas@users.noreply.github.com>
2018-03-30 18:34:39 +0200

    Some footer logo changes

--- templates/footer.php
+++ templates/footer.php
@@ -3,9 +3,9 @@
     <br>
     <div class="hidden-xs text-center">
       <div>
-        <a href="https://www.zuid-holland.nl/" target="_blank"><img class="right-footer-image" src="<?= get_template_directory_uri() . '/dist/images/PZH_groot_jpg.jpg'; ?>" alt="Provincie Zuid-Holland logo"></a>
-        <a href="https://openstate.eu/" style="" target="_blank"><img class="right-footer-image" src="<?= get_template_directory_uri() . '/dist/images/logo-open-state-black.svg'; ?>" alt="Open State Foundation logo"></a>
-        <a href="https://diggingfordata.nl/" target="_blank"><img class="right-footer-image" src="<?= get_template_directory_uri() . '/dist/images/Logo_digging_1.svg'; ?>" alt="Digging for Data"></a>
+        <a href="https://www.zuid-holland.nl/" target="_blank"><img class="right-footer-image" src="<?= get_template_directory_uri() . '/dist/images/provincie-zuid-holland-logo-witte-tekst.svg'; ?>" alt="Provincie Zuid-Holland logo"></a>
+        <a href="https://openstate.eu/" style="" target="_blank"><img class="right-footer-image" src="<?= get_template_directory_uri() . '/dist/images/logo-open-state-foundation.svg'; ?>" alt="Open State Foundation logo"></a>
+        <a href="https://diggingfordata.nl/" target="_blank"><img class="right-footer-image" src="<?= get_template_directory_uri() . '/dist/images/Logo_digging_1.svg'; ?>" alt="Digging for Data logo"></a>
       </div>
       <br>
       <div class="row">
@@ -28,17 +28,15 @@
     </div>
     <div class="visible-xs-block text-center">
       <div>
-        <a href="https://www.zuid-holland.nl/" target="_blank"><img class="right-footer-image" src="<?= get_template_directory_uri() . '/dist/images/PZH_groot_jpg.jpg'; ?>" alt="Provincie Zuid-Holland logo"></a>
-        <a href="https://www.rijksoverheid.nl/ministeries/ministerie-van-onderwijs-cultuur-en-wetenschap" target="_blank"><img class="right-footer-image" src="<?= get_template_directory_uri() . '/dist/images/Logo_ministerie_OCW.svg'; ?>" alt="Gemeente Amsterdam logo"></a>
-        <a href="https://openstate.eu/" target="_blank"><img class="right-footer-image" src="<?= get_template_directory_uri() . '/dist/images/logo-open-state-black.svg'; ?>" alt="Open State Foundation logo"></a>
-        <a href="https://youngcreators.co/" target="_blank"><img class="right-footer-image" src="<?= get_template_directory_uri() . '/dist/images/logo_young_creators.svg'; ?>" alt="Young Creators logo"></a>
+        <a href="https://www.zuid-holland.nl/" target="_blank"><img class="right-footer-image" src="<?= get_template_directory_uri() . '/dist/images/provincie-zuid-holland-logo-witte-tekst.svg'; ?>" alt="Provincie Zuid-Holland logo"></a>
+        <a href="https://openstate.eu/" style="" target="_blank"><img class="right-footer-image" src="<?= get_template_directory_uri() . '/dist/images/logo-open-state-foundation.svg'; ?>" alt="Open State Foundation logo"></a>
+        <a href="https://diggingfordata.nl/" target="_blank"><img class="right-footer-image" src="<?= get_template_directory_uri() . '/dist/images/Logo_digging_1.svg'; ?>" alt="Digging for Data logo"></a>
       </div>
       <br>
-      <img class="hdvs-logo float-left" src="<?= get_template_directory_uri() . '/dist/images/Logo_wit_kort-01.svg'; ?>" alt="Hack de Valse Start logo wit">
       <ul class="list-inline left-footer-list-padding">
         <li><a class="social left-footer-padding" href="https://www.facebook.com/openstate/" target="_blank"><i class="fa fa-facebook"></i></a></li>
         <li><a class="social left-footer-padding" href="https://github.com/openstate" target="_blank"><i class="fa fa-github"></i></a></li>
-        <li><a class="social hashtag-font-size" href="https://twitter.com/search?f=tweets&q=%23HdVS18" target="_blank">#DiggingforData</a></li>
+        <li><a class="social hashtag-font-size" href="https://twitter.com/search?f=tweets&q=%23DiggingforData" target="_blank">#DiggingforData</a></li>
       </ul>
     <br>
     </div>
