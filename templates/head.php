54ebd94a2a4f92ddeaea1e136c7ced6e5ee5431c
Sicco van Sas <siccovansas@users.noreply.github.com>
2018-03-31 21:16:57 +0200

    Change Open Graph and Analytics

--- templates/head.php
+++ templates/head.php
@@ -3,22 +3,22 @@
   <meta http-equiv="x-ua-compatible" content="ie=edge">
   <meta name="viewport" content="width=device-width, initial-scale=1">
   <meta name="author" content="Open State Foundation">
-  <meta property="og:title" content="Hack de Valse Start - 3 maart 2018 @ Calvijn College" />
+  <meta property="og:title" content="Digging for Data" />
   <meta property="og:type" content="website" />
-  <meta property="og:image" content="https://hackdevalsestart.nl/wp-content/themes/hack-de-valse-start-theme/dist/images/logo_hack_de_valse_start_og.png" />
-  <meta property="og:image:secure_url" content="https://hackdevalsestart.nl/wp-content/themes/hack-de-valse-start-theme/dist/images/logo_hack_de_valse_start_og.png" />
+  <meta property="og:image" content="https://diggingfordata.nl/wp-content/themes/digging-for-data-theme/dist/images/logo_og.png" />
+  <meta property="og:image:secure_url" content="https://diggingfordata.nl/wp-content/themes/digging-for-data-theme/dist/images/logo_og.png" />
   <meta property="og:image:type" content="image/png" />
   <meta property="og:image:width" content="1500" />
   <meta property="og:image:height" content="500" />
   <?php wp_head(); ?>
   <link href="https://fonts.googleapis.com/css?family=Source+Sans+Pro:400,400i,700" rel="stylesheet">
   <!-- Global site tag (gtag.js) - Google Analytics -->
-  <script async src="https://www.googletagmanager.com/gtag/js?id=UA-32274817-13"></script>
+  <script async src="https://www.googletagmanager.com/gtag/js?id=UA-32274817-15"></script>
   <script>
     window.dataLayer = window.dataLayer || [];
     function gtag(){dataLayer.push(arguments);}
     gtag('js', new Date());
-    gtag('config', 'UA-32274817-13', { 'anonymize_ip': true });
-    gtag('config', 'UA-32274817-13');
+    gtag('config', 'UA-32274817-15', { 'anonymize_ip': true });
+    gtag('config', 'UA-32274817-15');
   </script>
 </head>
